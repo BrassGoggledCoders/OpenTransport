a3a6616208c3815a3eea1502da2f8ecc73cd00bc
Skylar Sommers <ssommers@criticalmix.com>
2016-06-06 00:09:51 -0400

    Beginning of BlockContainer and player wrappers
    The longest road

---
+++ src/main/java/xyz/brassgoggledcoders/moarlibs/blocks/IBlockContainer.java
@@ -0,0 +1,5 @@
+package xyz.brassgoggledcoders.moarlibs.blocks;
+
+public class IBlockContainer
+{
+}
---
+++ src/main/java/xyz/brassgoggledcoders/moarlibs/wrappers/players/EntityPlayerMPWrapper.java
@@ -0,0 +1,18 @@
+package xyz.brassgoggledcoders.moarlibs.wrappers.players;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import xyz.brassgoggledcoders.moarlibs.blocks.IBlockContainer;
+
+public class EntityPlayerMPWrapper extends EntityPlayerMP
+{
+	protected EntityPlayerMP entityPlayer;
+	protected IBlockContainer blockContainer;
+
+	public EntityPlayerMPWrapper(EntityPlayerMP entityPlayer, IBlockContainer blockContainer)
+	{
+		super(entityPlayer.mcServer, entityPlayer.getServerWorld(), entityPlayer.getGameProfile(),
+				entityPlayer.interactionManager);
+		this.entityPlayer = entityPlayer;
+		this.blockContainer = blockContainer;
+	}
+}
