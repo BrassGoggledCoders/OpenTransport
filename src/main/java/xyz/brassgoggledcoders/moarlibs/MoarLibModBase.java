e84d8a8e6f6c4b1e788194d0a9a7112981294ea6
Skylar Sommers <ssommers@criticalmix.com>
2016-06-12 15:40:06 -0400

    Change MoarLibsModBase to force loading of ModuleHandler

--- src/main/java/xyz/brassgoggledcoders/moarlibs/MoarLibModBase.java
+++ src/main/java/xyz/brassgoggledcoders/moarlibs/MoarLibModBase.java
@@ -1,6 +1,7 @@
 package xyz.brassgoggledcoders.moarlibs;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import xyz.brassgoggledcoders.boilerplate.BoilerplateModBase;
 import xyz.brassgoggledcoders.moarlibs.api.IMoarRegister;
 import xyz.brassgoggledcoders.moarlibs.registries.BlockContainerRegistry;
@@ -13,8 +14,9 @@ public abstract class MoarLibModBase extends BoilerplateModBase
 	}
 
 	@Override
-	protected void afterModuleConstruct()
+	protected void afterModuleConstruct(FMLPreInitializationEvent event)
 	{
+		MoarLibs.INSTANCE.createHandlers(event);
 		this.getMoarRegister().registerItems(BlockContainerRegistry.getAllBlockContainers());
 		this.getMoarRegister().registerEntities();
 	}
