dbd9551812d4447b5800b4b4d11c9bd4d706d5b4
Skylar Sommers <ssommers@criticalmix.com>
2016-06-30 18:24:26 -0400

    Dock Block added

--- src/main/java/xyz/brassgoggledcoders/opentransport/modules/boats/navigation/NavigationModule.java
+++ src/main/java/xyz/brassgoggledcoders/opentransport/modules/boats/navigation/NavigationModule.java
@@ -4,6 +4,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import xyz.brassgoggledcoders.boilerplate.module.Module;
 import xyz.brassgoggledcoders.boilerplate.module.ModuleBase;
 import xyz.brassgoggledcoders.opentransport.OpenTransport;
+import xyz.brassgoggledcoders.opentransport.modules.boats.navigation.blocks.BlockDock;
 import xyz.brassgoggledcoders.opentransport.modules.boats.navigation.entities.EntityBuoy;
 import xyz.brassgoggledcoders.opentransport.modules.boats.navigation.items.ItemBuoy;
 
@@ -19,5 +20,6 @@ public class NavigationModule extends ModuleBase {
 	public void preInit(FMLPreInitializationEvent event) {
 		this.getEntityRegistry().registerEntity(EntityBuoy.class);
 		this.getItemRegistry().registerItem(new ItemBuoy());
+		this.getBlockRegistry().registerBlock(new BlockDock());
 	}
 }
---
+++ src/main/java/xyz/brassgoggledcoders/opentransport/modules/boats/navigation/blocks/BlockDock.java
@@ -0,0 +1,10 @@
+package xyz.brassgoggledcoders.opentransport.modules.boats.navigation.blocks;
+
+import net.minecraft.block.material.Material;
+import xyz.brassgoggledcoders.boilerplate.blocks.BlockBase;
+
+public class BlockDock extends BlockBase {
+	public BlockDock() {
+		super(Material.IRON, "dock");
+	}
+}
