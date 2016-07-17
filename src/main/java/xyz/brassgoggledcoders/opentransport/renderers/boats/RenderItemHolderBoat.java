1f0ffa52e7f395ca9880ac1ae6633fb5ea0a8f23
Skylar Sommers <ssommers@criticalmix.com>
2016-07-17 00:11:59 -0400

    Items now render
    Just need to pull it out for minecarts to use.

--- src/main/java/xyz/brassgoggledcoders/opentransport/renderers/boats/RenderItemHolderBoat.java
+++ src/main/java/xyz/brassgoggledcoders/opentransport/renderers/boats/RenderItemHolderBoat.java
@@ -2,7 +2,7 @@ package xyz.brassgoggledcoders.opentransport.renderers.boats;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
@@ -32,46 +32,50 @@ public class RenderItemHolderBoat implements IItemRenderingHandler {
 	}
 
 	@Override
-	public void render(World world, Item item, ItemStack itemStack, ItemCameraTransforms.TransformType type) {
+	public void render(World world, Item item, ItemStack itemStack, TransformType type) {
 		if(boatHolder == null)  {
 			boatHolder = new EntityBoatHolder(Minecraft.getMinecraft().theWorld);
 		}
 		if(boatHolder.getEntity().worldObj != null && item instanceof ItemBoatHolder) {
-			GlStateManager.pushMatrix();
 			ItemBoatHolder itemBoatHolder = (ItemBoatHolder)item;
 			IBlockContainer blockContainer = itemBoatHolder.getBlockContainer(itemStack);
 			blockContainer.setHolder(boatHolder);
 			boatHolder.setBlockContainer(blockContainer);
+
+			GlStateManager.pushMatrix();
+			GlStateManager.translate(.5, .55, .5);
 			switch(type) {
 				case GROUND:
-					GlStateManager.translate(.5, .55, .5);
 					GlStateManager.scale(.15, .15, .15);
-					renderBoat(itemStack.getItemDamage());
-					GlStateManager.rotate(90, 0, 1, 0);
-					GlStateManager.scale(1.5, 1.5, 1.5);
-					GlStateManager.translate(-.5, -0.25, .5);
-					renderBlockContainer(blockContainer);
 					break;
 				case GUI:
-					GlStateManager.translate(.5, .55, .5);
-					GlStateManager.scale(.15, .15, .15);
-					renderBoat(itemStack.getItemDamage());
-					GlStateManager.rotate(90, 0, 1, 0);
-					GlStateManager.scale(1.5, 1.5, 1.5);
-					GlStateManager.translate(-.5, -0.25, .5);
-					renderBlockContainer(blockContainer);
+					GlStateManager.scale(.25, .25, .25);
+					GlStateManager.rotate(45, 1, -1, 0);
 					break;
 				case FIRST_PERSON_LEFT_HAND:
+					GlStateManager.scale(.2, .2, .2);
+					GlStateManager.rotate(45, 1, 0, 0);
 					break;
 				case FIRST_PERSON_RIGHT_HAND:
+					GlStateManager.scale(.2, .2, .2);
+					GlStateManager.rotate(45, 1, 0, 0);
 					break;
 				case THIRD_PERSON_LEFT_HAND:
+					GlStateManager.scale(.15, .15, .15);
+					GlStateManager.rotate(45, 1, 0, 0);
 					break;
 				case THIRD_PERSON_RIGHT_HAND:
+					GlStateManager.scale(.15, .15, .15);
+					GlStateManager.rotate(45, 1, 0, 0);
 					break;
 				default:
 					break;
 			}
+			renderBoat(itemStack.getItemDamage());
+			GlStateManager.rotate(90, 0, 1, 0);
+			GlStateManager.scale(1.5, 1.5, 1.5);
+			GlStateManager.translate(-.5, -0.25, .5);
+			renderBlockContainer(blockContainer);
 			GlStateManager.popMatrix();
 		}
 	}
