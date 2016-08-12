04aba47b8737ff7f0e4807f8fb4e3425ec5aa5c5
Skylar Sommers <ssommers@criticalmix.com>
2016-08-11 23:11:50 -0400

    Added Reinforced Crate

--- src/main/java/xyz/brassgoggledcoders/opentransport/modules/immersiveengineering/IEBlockContainers.java
+++ src/main/java/xyz/brassgoggledcoders/opentransport/modules/immersiveengineering/IEBlockContainers.java
@@ -12,11 +12,18 @@ import xyz.brassgoggledcoders.opentransport.registries.BlockContainerRegistry;
 public class IEBlockContainers {
 	public static void preInit(FMLPreInitializationEvent event) {
 		BlockIEBase blockWoodenDevice0 = IEContent.blockWoodenDevice0;
-		IBlockContainer woodenStorageCrate = new BlockContainerBase(blockWoodenDevice0)
-				.setUnlocalizedName(blockWoodenDevice0.getUnlocalizedName() + "." + BlockTypes_WoodenDevice0.CRATE.getName())
-				.setBlockState(blockWoodenDevice0.getDefaultState().withProperty(blockWoodenDevice0.property,
-						BlockTypes_WoodenDevice0.CRATE)).setClickInteraction(new BlockActivationInteraction())
-				.setGuiInterface(new CrateInterface());
+		IBlockContainer woodenStorageCrate = new BlockContainerBase(blockWoodenDevice0).setUnlocalizedName(
+				blockWoodenDevice0.getUnlocalizedName() + "." + BlockTypes_WoodenDevice0.CRATE.getName()).setBlockState(
+				blockWoodenDevice0.getDefaultState()
+						.withProperty(blockWoodenDevice0.property, BlockTypes_WoodenDevice0.CRATE))
+				.setClickInteraction(new BlockActivationInteraction()).setGuiInterface(new CrateInterface());
 		BlockContainerRegistry.registerContainer(woodenStorageCrate);
+
+		IBlockContainer reinforcedWoodenStorageCrate = new BlockContainerBase(blockWoodenDevice0).setUnlocalizedName(
+				blockWoodenDevice0.getUnlocalizedName() + "." + BlockTypes_WoodenDevice0.REINFORCED_CRATE.getName())
+				.setBlockState(blockWoodenDevice0.getDefaultState()
+						.withProperty(blockWoodenDevice0.property, BlockTypes_WoodenDevice0.REINFORCED_CRATE))
+				.setClickInteraction(new BlockActivationInteraction()).setGuiInterface(new CrateInterface());
+		BlockContainerRegistry.registerContainer(reinforcedWoodenStorageCrate);
 	}
 }
