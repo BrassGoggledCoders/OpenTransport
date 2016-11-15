8fe44bc5db1ceb6036cfd09deae5ec89d28613d0
Skylar Sommers <ssommers@criticalmix.com>
2016-11-14 22:20:32 -0500

    Modularutilities now loads it's module

--- src/main/java/xyz/brassgoggledcoders/opentransport/modules/modularutilities/ModularUtilitiesModule.java
+++ src/main/java/xyz/brassgoggledcoders/opentransport/modules/modularutilities/ModularUtilitiesModule.java
@@ -1,12 +1,15 @@
 package xyz.brassgoggledcoders.opentransport.modules.modularutilities;
 
+import com.teamacronymcoders.base.modulesystem.Module;
 import com.teamacronymcoders.base.modulesystem.ModuleBase;
 import com.teamacronymcoders.base.modulesystem.dependencies.IDependency;
 import com.teamacronymcoders.base.modulesystem.dependencies.ModDependency;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import xyz.brassgoggledcoders.opentransport.OpenTransport;
 
 import java.util.List;
 
+@Module(OpenTransport.MODID)
 public class ModularUtilitiesModule extends ModuleBase {
     @Override
     public String getName() {
