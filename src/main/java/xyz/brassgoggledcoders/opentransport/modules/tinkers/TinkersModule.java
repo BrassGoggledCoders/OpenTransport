f317c02fcf0fd5ac249c8c61b8a543aedde6be5e
Skylar Sommers <ssommers@criticalmix.com>
2016-11-16 09:52:58 -0500

    First five carts for Tinker's

---
+++ src/main/java/xyz/brassgoggledcoders/opentransport/modules/tinkers/TinkersModule.java
@@ -0,0 +1,31 @@
+package xyz.brassgoggledcoders.opentransport.modules.tinkers;
+
+import com.teamacronymcoders.base.modulesystem.Module;
+import com.teamacronymcoders.base.modulesystem.dependencies.IDependency;
+import com.teamacronymcoders.base.modulesystem.dependencies.ModDependency;
+import xyz.brassgoggledcoders.opentransport.OpenTransport;
+import xyz.brassgoggledcoders.opentransport.api.blockwrappers.IHasWrappers;
+import xyz.brassgoggledcoders.opentransport.modulesystem.ModuleWrapperBase;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+@Module(OpenTransport.MODID)
+public class TinkersModule extends ModuleWrapperBase {
+    @Override
+    public String getName() {
+        return "Tinkers' Construct";
+    }
+
+    @Override
+    public List<IDependency> getDependencies(List<IDependency> dependencies) {
+        dependencies.add(new ModDependency("tconstruct"));
+        return dependencies;
+    }
+
+    @Nonnull
+    @Override
+    public IHasWrappers getWrapperHolder() {
+        return new TinkersBlockWrappers();
+    }
+}
