55e85bceac6de13b86950509e8df8cd5629ec6eb
Skylar Sommers <ssommers@criticalmix.com>
2016-11-30 10:13:45 -0500

    Added new ModModuleDependency

--- src/main/java/xyz/brassgoggledcoders/opentransport/modules/modularutilities/ModularUtilitiesModule.java
+++ src/main/java/xyz/brassgoggledcoders/opentransport/modules/modularutilities/ModularUtilitiesModule.java
@@ -2,15 +2,25 @@ package xyz.brassgoggledcoders.opentransport.modules.modularutilities;
 
 import com.teamacronymcoders.base.modulesystem.Module;
 import com.teamacronymcoders.base.modulesystem.ModuleBase;
+import com.teamacronymcoders.base.modulesystem.dependencies.IDependency;
+import com.teamacronymcoders.base.modulesystem.dependencies.ModModuleDependency;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import xyz.brassgoggledcoders.opentransport.OpenTransport;
+
+import java.util.List;
 
 @Module(OpenTransport.MODID)
 public class ModularUtilitiesModule extends ModuleBase {
     @Override
     public String getName() {
         return "Modular Utilities";
+    }
+
+    @Override
+    public List<IDependency> getDependencies(List<IDependency> dependencies) {
+        dependencies.add(new ModModuleDependency("modularutilities", "Ender"));
+        return dependencies;
     }
 
     @Override
