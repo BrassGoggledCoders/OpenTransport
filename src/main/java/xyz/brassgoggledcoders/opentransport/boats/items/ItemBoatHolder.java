e32847030c8f5542dede7c1c1f528c315ac373e7
Skylar Sommers <ssommers@criticalmix.com>
2017-05-24 00:17:43 -0400

    Use ItemBoatBase

--- src/main/java/xyz/brassgoggledcoders/opentransport/boats/items/ItemBoatHolder.java
+++ src/main/java/xyz/brassgoggledcoders/opentransport/boats/items/ItemBoatHolder.java
@@ -1,132 +1,38 @@
 package xyz.brassgoggledcoders.opentransport.boats.items;
 
 import com.teamacronymcoders.base.client.models.IHasModel;
-import net.minecraft.block.Block;
+import com.teamacronymcoders.base.items.boats.ItemBoatBase;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityBoat.Type;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBoat;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.stats.StatBase;
 import net.minecraft.stats.StatList;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.NonNullList;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import xyz.brassgoggledcoders.opentransport.api.OpenTransportAPI;
 import xyz.brassgoggledcoders.opentransport.api.wrappers.block.IBlockWrapper;
 import xyz.brassgoggledcoders.opentransport.boats.BoatTransport;
 import xyz.brassgoggledcoders.opentransport.boats.entities.EntityBoatHolder;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
 import java.util.List;
 
-public class ItemBoatHolder extends ItemBoat implements IHasModel {
+public class ItemBoatHolder extends ItemBoatBase<EntityBoatHolder> implements IHasModel {
     boolean creativeTabSet = false;
 
     public ItemBoatHolder(CreativeTabs tab) {
-        super(Type.OAK);
-        this.setUnlocalizedName("boat.holder");
+        super("boat.holder");
         this.setCreativeTab(tab);
     }
 
     @Override
     @Nonnull
-    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer entityPlayer, @Nonnull EnumHand hand) {
-        ItemStack itemStack = entityPlayer.getHeldItem(hand);
-        float f = 1.0F;
-        float f1 = entityPlayer.prevRotationPitch + (entityPlayer.rotationPitch - entityPlayer.prevRotationPitch) * f;
-        float f2 = entityPlayer.prevRotationYaw + (entityPlayer.rotationYaw - entityPlayer.prevRotationYaw) * f;
-        double d0 = entityPlayer.prevPosX + (entityPlayer.posX - entityPlayer.prevPosX) * (double) f;
-        double d1 =
-                entityPlayer.prevPosY + (entityPlayer.posY - entityPlayer.prevPosY) * (double) f + (double) entityPlayer
-                        .getEyeHeight();
-        double d2 = entityPlayer.prevPosZ + (entityPlayer.posZ - entityPlayer.prevPosZ) * (double) f;
-        Vec3d vec3d = new Vec3d(d0, d1, d2);
-        float f3 = MathHelper.cos(-f2 * 0.017453292F - (float) Math.PI);
-        float f4 = MathHelper.sin(-f2 * 0.017453292F - (float) Math.PI);
-        float f5 = -MathHelper.cos(-f1 * 0.017453292F);
-        float f6 = MathHelper.sin(-f1 * 0.017453292F);
-        float f7 = f4 * f5;
-        float f8 = f3 * f5;
-        double d3 = 5.0D;
-        Vec3d vec3d1 = vec3d.addVector((double) f7 * d3, (double) f6 * d3, (double) f8 * d3);
-        RayTraceResult raytraceresult = world.rayTraceBlocks(vec3d, vec3d1, true);
-
-        if (raytraceresult == null) {
-            return new ActionResult<>(EnumActionResult.PASS, itemStack);
-        } else {
-            Vec3d vec3d2 = entityPlayer.getLook(f);
-            boolean flag = false;
-            List<Entity> list = world.getEntitiesWithinAABBExcludingEntity(entityPlayer,
-                    entityPlayer.getEntityBoundingBox()
-                            .addCoord(vec3d2.xCoord * d3, vec3d2.yCoord * d3, vec3d2.zCoord * d3).expandXyz(1.0D));
-
-            for (Entity entity : list) {
-                if (entity.canBeCollidedWith()) {
-                    AxisAlignedBB axisalignedbb =
-                            entity.getEntityBoundingBox().expandXyz((double) entity.getCollisionBorderSize());
-
-                    if (axisalignedbb.isVecInside(vec3d)) {
-                        flag = true;
-                    }
-                }
-            }
-
-            if (flag) {
-                return new ActionResult<>(EnumActionResult.PASS, itemStack);
-            } else if (raytraceresult.typeOfHit != RayTraceResult.Type.BLOCK) {
-                return new ActionResult<>(EnumActionResult.PASS, itemStack);
-            } else {
-                Block block = world.getBlockState(raytraceresult.getBlockPos()).getBlock();
-                boolean isWater = block == Blocks.WATER || block == Blocks.FLOWING_WATER;
-                EntityBoatHolder entityBoatHolder = new EntityBoatHolder(world);
-                double boatPosX = raytraceresult.hitVec.xCoord;
-                double boatPosY = isWater ? raytraceresult.hitVec.yCoord - 0.12D : raytraceresult.hitVec.yCoord;
-                double boatPosZ = raytraceresult.hitVec.zCoord;
-                entityBoatHolder.setPosition(boatPosX, boatPosY, boatPosZ);
-                entityBoatHolder.setBoatType(this.getType(itemStack));
-                entityBoatHolder.setItemBoat(itemStack);
-                entityBoatHolder.setBlockWrapper(this.getBlockWrapper(itemStack).copy());
-                entityBoatHolder.rotationYaw = entityPlayer.rotationYaw;
-
-                if (!world.getCollisionBoxes(entityBoatHolder, entityBoatHolder.getEntityBoundingBox().expandXyz(-0.1D))
-                        .isEmpty()) {
-                    return new ActionResult<>(EnumActionResult.FAIL, itemStack);
-                } else {
-                    if (!entityPlayer.capabilities.isCreativeMode) {
-                        itemStack.shrink(1);
-                    }
-
-                    if (!world.isRemote) {
-                        world.spawnEntity(entityBoatHolder);
-                        entityBoatHolder.getBlockWrapper().onPlace(entityPlayer, hand, itemStack);
-                    }
-
-                    this.increaseStat(entityPlayer);
-                    return new ActionResult<>(EnumActionResult.SUCCESS, itemStack);
-                }
-            }
-        }
-    }
-
-    @Override
-    @Nonnull
-    public String getUnlocalizedName(ItemStack itemStack) {
+    public String getUnlocalizedName(@Nonnull ItemStack itemStack) {
         return this.getUnlocalizedName() + "." + this.getType(itemStack).toString().toLowerCase();
     }
 
@@ -145,13 +51,6 @@ public class ItemBoatHolder extends ItemBoat implements IHasModel {
         return displayName;
     }
 
-    public void increaseStat(EntityPlayer entityPlayer) {
-        StatBase stat = StatList.getObjectUseStats(this);
-        if (stat != null) {
-            entityPlayer.addStat(stat);
-        }
-    }
-
     @Override
     @Nonnull
     public Item setCreativeTab(@Nonnull CreativeTabs tab) {
@@ -166,8 +65,17 @@ public class ItemBoatHolder extends ItemBoat implements IHasModel {
         return OpenTransportAPI.getBlockWrapperRegistry().getLoadedBlockWrapper(itemStack);
     }
 
-    public Type getType(ItemStack itemStack) {
-        return Type.values()[itemStack.getItemDamage()];
+    @Override
+    public EntityBoatHolder getBoatToPlace(World world, ItemStack itemStack) {
+        EntityBoatHolder entityBoatHolder = new EntityBoatHolder(world);
+        entityBoatHolder.setItemBoat(itemStack);
+        entityBoatHolder.setBlockWrapper(this.getBlockWrapper(itemStack).copy());
+        return entityBoatHolder;
+    }
+
+    @Override
+    public void onPlace(EntityBoatHolder entityBoatHolder, EntityPlayer entityPlayer, EnumHand hand, ItemStack itemStack) {
+        entityBoatHolder.getBlockWrapper().onPlace(entityPlayer, hand, itemStack);
     }
 
     public Item getBoatItem(ItemStack itemStack) {
@@ -191,18 +99,6 @@ public class ItemBoatHolder extends ItemBoat implements IHasModel {
                 break;
         }
         return itemBoat;
-    }
-
-    @Override
-    public List<String> getModelNames(List<String> modelNames) {
-        modelNames.add("boat.holder");
-        return modelNames;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void getSubItems(@Nonnull Item item, CreativeTabs tab, NonNullList<ItemStack> itemStacks) {
-        itemStacks.addAll(this.getAllSubItems(new ArrayList<>()));
     }
 
     @Override
