abd53beb301cef532a41d9a9fa15d6799b4b536c
SkySom <skylar.som@gmail.com>
2017-10-28 11:35:39 -0400

    Finish setting up the module

--- src/main/java/xyz/brassgoggledcoders/opentransport/vehicle/minecart/ModuleMinecart.java
+++ src/main/java/xyz/brassgoggledcoders/opentransport/vehicle/minecart/ModuleMinecart.java
@@ -1,17 +1,22 @@
 package xyz.brassgoggledcoders.opentransport.vehicle.minecart;
 
+import com.teamacronymcoders.base.creativetabs.CreativeTabBase;
 import com.teamacronymcoders.base.modulesystem.Module;
 import com.teamacronymcoders.base.modulesystem.ModuleBase;
 import com.teamacronymcoders.base.registrysystem.EntityRegistry;
 import com.teamacronymcoders.base.registrysystem.ItemRegistry;
 import com.teamacronymcoders.base.registrysystem.config.ConfigRegistry;
-import net.minecraftforge.fml.common.registry.EntityEntry;
-import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import xyz.brassgoggledcoders.opentransport.vehicle.minecart.entity.EntityMinecartCarrier;
 import xyz.brassgoggledcoders.opentransport.vehicle.minecart.item.ItemMinecartCarrier;
 
 @Module("vehicle")
 public class ModuleMinecart extends ModuleBase {
+    public static final CreativeTabs MINECART_CARRIERS = new CreativeTabBase("carrier.minecart",
+            () -> new ItemStack((Items.MINECART)));
+
     @Override
     public String getName() {
         return "minecart";
@@ -19,7 +24,7 @@ public class ModuleMinecart extends ModuleBase {
 
     @Override
     public void registerItems(ConfigRegistry configRegistry, ItemRegistry itemRegistry) {
-        itemRegistry.register(new ItemMinecartCarrier());
+        itemRegistry.register(new ItemMinecartCarrier().setCreativeTab(MINECART_CARRIERS));
     }
 
     @Override
